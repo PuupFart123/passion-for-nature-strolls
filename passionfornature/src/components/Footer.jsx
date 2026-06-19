a2a143895183ddbb1dc3f58571fc3e114e95b40a
PuupFart123 <wallabysofa@gmail.com>
2026-06-18 22:08:19 -0700

    debug

--- passionfornature/src/components/Footer.jsx
+++ passionfornature/src/components/Footer.jsx
@@ -66,17 +66,17 @@ const Footer = () => {
               <p>Sammamish, Washington</p>
             </div>
             <div className="footer-social">
-              <a href="#" className="social-link" aria-label="Facebook">
+              <button type="button" className="social-link" aria-label="Facebook">
                 <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
                   <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
                 </svg>
-              </a>
-              <a href="#" className="social-link" aria-label="Instagram">
+              </button>
+              <button type="button" className="social-link" aria-label="Instagram">
                 <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
                   <path d="M12.017 0C5.396 0 .029 5.367.029 11.987c0 6.62 5.367 11.987 11.988 11.987 6.62 0 11.987-5.367 11.987-11.987C24.014 5.367 18.637.001 12.017.001zM8.449 16.988c-1.297 0-2.448-.49-3.323-1.297C4.198 14.895 3.708 13.744 3.708 12.447s.49-2.448 1.297-3.323c.875-.807 2.026-1.297 3.323-1.297s2.448.49 3.323 1.297c.807.875 1.297 2.026 1.297 3.323s-.49 2.448-1.297 3.323c-.875.807-2.026 1.297-3.323 1.297zm7.83-9.781c-.49 0-.927-.175-1.297-.49-.37-.315-.49-.807-.49-1.297s.12-.982.49-1.297c.37-.315.807-.49 1.297-.49s.927.175 1.297.49c.37.315.49.807.49 1.297s-.12.982-.49 1.297c-.37.315-.807.49-1.297.49z"/>
                 </svg>
-              </a>
-              <a href="#" className="social-link" aria-label="Email">
+              </button>
+              <a href="mailto:rolando_jordan@hotmail.com" className="social-link" aria-label="Email">
                 <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
                   <path d="M0 3v18h24V3H0zm21.518 2L12 12.713 2.482 5h19.036zM2 19V7.183l10 8.104 10-8.104V19H2z"/>
                 </svg>
